e646512a4f167281a7fd3a29a861f7b2ae7b4814
sq1fya <rgpszczecin@gmail.com>
2018-10-13 11:59:58 +0200

    WIP: Moving task to state to update them

--- src/App.js
+++ src/App.js
@@ -1,44 +1,47 @@
-import React, { Component } from 'react';
-import './App.css';
-
-const users = [
-  {
-    id: 1,
-    title: 'User 1',
-    isDone: false,
-    isImportant: false
-  },
-  {
-    id: 2,
-    title: 'User 2',
-    isDone: true,
-    isImportant: false
-  },
-  {
-    id: 3,
-    title: 'User 3',
-    isDone: false,
-    isImportant: true
-  }
-]
-
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
+
+state = {
+
+  tasks: [
+    {
+      id: 1,
+      title: 'User 1',
+      isDone: false,
+      isImportant: false
+    },
+    {
+      id: 2,
+      title: 'User 2',
+      isDone: true,
+      isImportant: false
+    },
+    {
+      id: 3,
+      title: 'User 3',
+      isDone: false,
+      isImportant: true
+    }
+  ]
+}
+
   render() {
     return (
       <div className="App">
         <h1>Menu</h1>
         <ul>
           {
-            users.map(
-              user => (
-                <li key={user.id}>
-                {
-                  user.isImportant && <span>&#9734;</span>
-                }
-                {
-                  user.isDone ? <del>[user.title]</del> : user.title
-                }
+            this.state.tasks.map(
+              task => (
+                <li key={task.id}>
+                  {
+                    task.isImportant ? <span>&#9734;</span> : <span>&#9733;</span>
+                  }
+                  {
+                    task.isDone ? <del>{task.title}</del> : task.title
+                  }
                 </li>
               )
             )
